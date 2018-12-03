df60f7fdcefe5373e39cec353141ddea5f80396c
Wei Cong <weicong@Weis-MacBook-Pro.local>
2018-12-03 20:28:35 +0800

    Updated sample files

--- sample_config.js
+++ sample_config.js
@@ -2,7 +2,8 @@ module.exports = {
  api_url: "http://datamall2.mytransport.sg/ltaodataservice/",
  AccountKey: "",
  buses: [],
- offsetArrival: 30 * 1000,
  firststop_cron: "*/20 * * * * *",
- nextstop_cron: "*/20 * * * * *"
+ nextstop_cron: "*/20 * * * * *",
+ reset_cron: "*/3 * * * *",
+ stopsDataPath: "./exported_data/[bus]_busstops_raw.csv"
 }
